a772cf0a93c9dfc626a6cc234f185998d612d4de
Will Morris <willmorris103@gmail.com>
2025-09-12 17:54:27 -0500

    Refactor configuration constants and import order for improved organization and clarity

--- config/__init__.py
+++ config/__init__.py
@@ -2,7 +2,14 @@ from .constants import (
     MD_DB,
     REPO_DATABASE,
     MD_VAULT,
-    IGNORE_PARTS,
-    IGNORE_EXTENSIONS,
-    MD_XREF,
+    STORAGE,
+    EMBED_MODEL_ID,
+    EMBED_MODEL_NAME,
+    EMBEDDING_LENGTH,
+    MAX_TOKENS
 )
+
+from ignore_ext import IGNORE_EXTENSIONS
+from ignore_parts import IGNORE_PARTS
+from md_xref import MD_XREF
+from headers import HEADERS
--- config/constants.py
+++ config/constants.py
@@ -5,31 +5,29 @@ from pathlib import Path
 from dotenv import load_dotenv
 from sqlite_utils import Database
 
-from ignore_ext import IGNORE_EXTENSIONS
-from ignore_parts import IGNORE_PARTS
-from md_xref import MD_XREF
 
 IS_INITIALIZED = False
-IS_LOADED = False
 
-_this_dir = Path(__file__).resolve().parent
-_app_data_dir = os.getenv("APP_DATA_DIR", _this_dir.parent / "data")
-_app_data_dir = Path(_app_data_dir).resolve()
+_root_dir = Path(__file__).resolve().parent.parent
+_app_data_dir = os.getenv("APP_DATA_DIR", _root_dir.parent / "data")
+_storage = os.getenv("STORAGE", _app_data_dir)
+_app_data_dir = Path(_storage).resolve() if _storage else Path(_app_data_dir).resolve()
 
 _host = os.getenv("COMPUTERNAME", "unknown")
 _user = os.getenv("USERNAME", "unknown")
 
-# private constants for setup
-_app_dotenv = _this_dir.parent / ".env"
+# setup db paths
+_md_db = _app_data_dir / "md.db"
+_repo_db = _app_data_dir / "repo.db"
 
+# setup vault dir
+_md_vault = _app_data_dir / "md_vault"
 
-_default_repos_db = os.getenv("REPO_DB", _app_data_dir / "repos.db")
-_md_db = os.getenv("MD_DB", _app_data_dir / "md.db")
-_md_vault = os.getenv("MD_VAULT", _app_data_dir / "md_vault")
-_ignore_parts_path = os.getenv("IGNORE_PARTS_PATH", _app_data_dir / "ignore_parts.json")
-_ignore_ext_path = os.getenv("IGNORE_EXT_PATH", _app_data_dir / "ignore_ext.json")
-_md_xref = os.getenv("MD_XREF", _app_data_dir / "md_xref.json")
-_md_xref = Path(_md_xref).resolve()
+# setup config files
+_app_dotenv = _root_dir.parent / ".env"
+_ignore_parts_path = _app_data_dir / "ignore_parts.json"
+_ignore_ext_path = _app_data_dir / "ignore_ext.json"
+_md_xref = _app_data_dir / "md_xref.json"
 
 
 def _init_config():
@@ -38,7 +36,7 @@ def _init_config():
         return
     Path(_app_data_dir).mkdir(parents=True, exist_ok=True)
     Path(_app_dotenv).touch(exist_ok=True)
-    Path(_default_repos_db).touch(exist_ok=True)
+    Path(_repo_db).touch(exist_ok=True)
     Path(_md_db).touch(exist_ok=True)
     IS_INITIALIZED = True
 
@@ -49,85 +47,39 @@ if not IS_INITIALIZED:
 
 load_dotenv(_app_dotenv)
 
+# sqlite-utils Databases
 MD_DB = Database(_md_db)
-REPO_DATABASE = Database(_default_repos_db)
-MD_VAULT = Path(_md_vault).resolve()
-REPO_DATABASE = Path(_default_repos_db).resolve()
-IGNORE_PARTS_CONFIG = Path(_ignore_parts_path).resolve()
-IGNORE_EXTENSIONS_CONFIG = Path(_ignore_ext_path).resolve()
-MD_XREF_CONFIG = Path(_md_xref).resolve()
+REPO_DATABASE = Database(_repo_db)
 
+# data dirs
+STORAGE = _app_data_dir
+MD_VAULT = _md_vault
+IGNORE_PARTS_CONFIG = _ignore_parts_path
+IGNORE_EXTENSIONS_CONFIG = _ignore_ext_path
+MD_XREF_CONFIG = _md_xref
+
+
+MAX_TOKENS = 2048
+EMBEDDING_LENGTH = 768
+EMBED_MODEL_ID = "nomic-ai/nomic-embed-text-v1.5"
+EMBED_MODEL_NAME = "nomic-embed-text"
 
 OBSIDIAN_EXE = f"C:\\Users\\{_user}\\AppData\\Local\\Programs\\Obsidian\\Obsidian.exe"
-
-
-def _load_md_xref() -> None:
-    IGNORE_EXTENSIONS_CONFIG.parent.mkdir(parents=True, exist_ok=True)
-    if not MD_XREF_CONFIG.exists():
-        with open(MD_XREF_CONFIG, "w", encoding="utf-8") as f:
-            json.dump(sorted(MD_XREF), f, indent=2)
-    else:
-        with open(MD_XREF_CONFIG, "r", encoding="utf-8") as f:
-            try:
-                data = json.load(f)
-                if isinstance(data, list):
-                    MD_XREF.update(data)
-            except json.JSONDecodeError:
-                # if file corrupt/empty, rewrite defaults
-                with open(MD_XREF_CONFIG, "w", encoding="utf-8") as out:
-                    json.dump(sorted(MD_XREF), out, indent=2)
-
-
-def _load_ignore_parts() -> None:
-    IGNORE_PARTS_CONFIG.parent.mkdir(parents=True, exist_ok=True)
-    if not IGNORE_PARTS_CONFIG.exists():
-        with open(IGNORE_PARTS_CONFIG, "w", encoding="utf-8") as f:
-            json.dump(sorted(IGNORE_PARTS), f, indent=2)
-    else:
-        with open(IGNORE_PARTS_CONFIG, "r", encoding="utf-8") as f:
-            try:
-                data = json.load(f)
-                if isinstance(data, list):
-                    IGNORE_PARTS.update(data)
-            except json.JSONDecodeError:
-                # if file corrupt/empty, rewrite defaults
-                with open(IGNORE_PARTS_CONFIG, "w", encoding="utf-8") as out:
-                    json.dump(sorted(IGNORE_PARTS), out, indent=2)
-
-
-def _load_ignore_extensions() -> None:
-    IGNORE_EXTENSIONS_CONFIG.parent.mkdir(parents=True, exist_ok=True)
-    if not IGNORE_EXTENSIONS_CONFIG.exists():
-        with open(IGNORE_EXTENSIONS_CONFIG, "w", encoding="utf-8") as f:
-            json.dump(sorted(IGNORE_EXTENSIONS), f, indent=2)
-    else:
-        with open(IGNORE_EXTENSIONS_CONFIG, "r", encoding="utf-8") as f:
-            try:
-                data = json.load(f)
-                if isinstance(data, list):
-                    IGNORE_EXTENSIONS.update(data)
-            except json.JSONDecodeError:
-                with open(IGNORE_EXTENSIONS_CONFIG, "w", encoding="utf-8") as out:
-                    json.dump(sorted(IGNORE_EXTENSIONS), out, indent=2)
 
 
 def print_config():
     print(
         f"""
-_this_dir: {_this_dir}
+_root_dir: {_root_dir}
 _app_data_dir: {_app_data_dir}
 _host: {_host}
 _user: {_user}
-_md_xref: {_md_xref}
 _md_vault: {_md_vault}
+_md_xref: {_md_xref}
 _ignore_parts_path: {_ignore_parts_path}
 _ignore_ext_path: {_ignore_ext_path}
 """
     )
 
 
-if not IS_LOADED:
-    _load_ignore_extensions()
-    _load_ignore_parts()
-    _load_md_xref()
-    IS_LOADED = True
+print_config()
