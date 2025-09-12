c18a009e5278e773391630953db7f9ca0a788a24
Will Morris <willmorris103@gmail.com>
2025-09-12 18:40:50 -0500

    Refactor Config class: update database path attributes, enhance computed fields for database connections, and improve overall structure

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,10 @@
-from .constants import (
+from sqlalchemy import create_engine, Engine
+from pathlib import Path
+from typing import Any
+
+from pydantic import computed_field
+from sqlite_utils import Database
+from constants import (
     MD_DB,
     REPO_DATABASE,
     MD_VAULT,
@@ -12,6 +18,8 @@ from .constants import (
     HOST,
     USER,
     POSTGRES_URI,
+    _md_db,
+    _repo_db
 )
 from ignore_ext import IGNORE_EXTENSIONS
 from ignore_parts import IGNORE_PARTS
@@ -22,10 +30,10 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-    md_db: str = SQLITE_MD_URI
-    repo_db: str = SQLITE_REPO_URI
-    storage: str = STORAGE
-    md_vault: str = MD_VAULT
+    repo_db_pth: str = _repo_db.as_posix()
+    md_db_pth: str = _md_db.as_posix()
+    storage: Path = STORAGE
+    md_vault: Path = MD_VAULT
     ignore_parts: list[str] = IGNORE_PARTS
     ignore_extensions: list[str] = IGNORE_EXTENSIONS
     md_xref: dict[str, str] = MD_XREF
@@ -34,13 +42,38 @@ class Config(BaseSettings):
     embed_model_name: str = EMBED_MODEL_NAME
     embedding_length: int = EMBEDDING_LENGTH
     max_tokens: int = MAX_TOKENS
-    sl_utils_repo_db: str = REPO_DATABASE
-    sl_utils_md_db: str = MD_DB
     host: str = HOST
     user: str = USER
-    postgres_uri: str = POSTGRES_URI
+    postgres_uri: str | None = POSTGRES_URI
+    repo_db_uri: str = SQLITE_REPO_URI
+    md_db_uri: str = SQLITE_MD_URI
 
     model_config = {
-        "arbitrary_types_allowed": True,
-        "from_attributes": True
     }
+
+    @computed_field
+    def md_db(self) -> Database:
+        return Database(self.md_db_pth)
+
+    @computed_field
+    def repo_db(self) -> Database:
+        return Database(self.repo_db_pth)
+
+    @computed_field
+    def postgres_db(self) -> Engine | None:
+
+        if self.postgres_uri:
+            return create_engine(self.postgres_uri)
+        return None
+
+
+config = Config()
+print(config.md_db.table_names())
+
+eng = config.postgres_db
+if eng is not None:
+    with eng.connect() as conn:
+        result = conn.execute("SELECT 1")
+        print(result.fetchone())
+else:
+    print("Postgres database is not available.")
